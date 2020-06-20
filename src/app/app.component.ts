e46125fce97cb6f7e4b6addc64c3c0412ded272a
Ross Moug <ross.moug@gmail.com>
2020-06-20 21:53:16 +0100

    Amplify getting started tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService } from './API.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amplify-tutorial';
+  todos: any[] = [];
+
+  constructor(private apiService: APIService) {}
+
+  ngOnInit(): void {
+    this.apiService.ListTodos().then((evt) => {
+      this.todos = evt.items;
+    });
+
+    this.apiService.OnCreateTodoListener.subscribe((evt) => {
+      const data = (evt as any).value.data.onCreateTodo;
+      this.todos = [...this.todos, data];
+    });
+  }
+
+  createTodo(): void {
+    this.apiService.CreateTodo({
+        name: 'Angular',
+        description: 'testing'
+    });
+  }
 }
